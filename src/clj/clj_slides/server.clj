be932c6107227ca99834aaa09e5f578a7a18fd0e
jls83 <josephlsanders@gmail.com>
2021-08-26 11:10:53 -0400

    Add arg parser, use for port

--- src/clj/clj_slides/server.clj
+++ src/clj/clj_slides/server.clj
@@ -2,9 +2,19 @@
     (:require
      [clj-slides.handler :refer [app]]
      [config.core :refer [env]]
-     [ring.adapter.jetty :refer [run-jetty]])
+     [ring.adapter.jetty :refer [run-jetty]]
+     [clojure.tools.cli :refer [parse-opts]])
     (:gen-class))
 
+(def cli-options
+  [["-i" "--input FILE" "Slides file"
+    :default "blah.md"]
+   ["-p" "--port PORT" "Port number"
+    :default 3000
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be between 0 and 65536"]]])
+
 (defn -main [& args]
-  (let [port (or (env :port) 3000)]
+  (let [parsed-opts (parse-opts args cli-options)
+        port (get-in parsed-opts [:options :port])]
     (run-jetty #'app {:port port :join? false})))
